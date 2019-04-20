9ee470f1f407bfa0381569a11532da8010b357ba
Jiawei Wang <jwang96@uab.edu>
2019-04-20 17:13:21 -0500

    added automatic crawler

--- test_search.py
+++ test_search.py
@@ -24,8 +24,36 @@ def getTweets(team_name="nyxl", time_since="2019-02-15", time_until="2019-02-16"
         team_name).setSince(time_since).setUntil(time_until)
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
     for tweet in tweets:
-        printTweet("%s_%s_%s.json" %
+        printTweet("data/%s_%s_%s.json" %
                    (team_name, time_since, time_until), tweet, lang=lang)
 
 
-getTweets()
+time_stage1 = [
+    ["2019-02-14","2019-02-18"],
+    ["2019-02-21","2019-02-25"],
+    ["2019-02-28","2019-03-04"],
+    ["2019-03-07","2019-03-11"],
+    ["2019-03-16","2019-03-18"],
+    ["2019-03-21","2019-03-22"]
+]
+time_stage2 = [
+    ["2019-04-04","2019-04-08"],
+    ["2019-04-11","2019-04-15"],
+    ["2019-04-18","2019-04-20"],
+]
+stages=[time_stage1,time_stage2]
+teams=["Chengdu Hunters","Dallas Fuel",
+"Guangzhou Charge","Hangzhou Spark",
+"Los Angeles Gladiators","Los Angeles Valiant",
+"SFShock","Seoul Dynasty",
+"Shanghai Dragons","Vancouver Titans"]
+
+for team in teams:
+    print("Crawling teams: ",team)
+    for i,time_stage in enumerate(stages):
+        print("  Stage: ",(i+1))
+        for week,date in enumerate(time_stage):
+            print("    Week: ",(week+1))
+            getTweets(team_name=team,time_since=date[0],time_until=date[1])
+
+
